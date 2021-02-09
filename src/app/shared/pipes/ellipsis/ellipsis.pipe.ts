fb501ead915114795a4b8106d6285291dcbebbfc
Ron Kaminski <ronka478@gmail.com>
2021-02-09 20:54:34 +0200

    stam for now

--- src/app/shared/pipes/ellipsis/ellipsis.pipe.ts
+++ src/app/shared/pipes/ellipsis/ellipsis.pipe.ts
@@ -5,12 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class EllipsisPipe implements PipeTransform {
 
-  transform(value: string, count: number = 30): string {
-    let retTxt = value;
-    if (retTxt && retTxt.length > count) {
-      return retTxt.substring(0, count - 3) + "...";
-    }
-    return retTxt;
+  transform(value: string): string {
+    return `<span design="subtitle">${value}</span>`;
   }
 
 }
