de29b57d57341386ab830ac2df08e28057cfa850
Ron Kaminski <ronka478@gmail.com>
2021-02-24 16:17:58 +0200

    cli update

--- src/app/features/website/questions-management/manager-page/manager-page.component.spec.ts
+++ src/app/features/website/questions-management/manager-page/manager-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ManagerPageComponent } from './manager-page.component';
 
@@ -6,7 +6,7 @@ describe('ManagerPageComponent', () => {
   let component: ManagerPageComponent;
   let fixture: ComponentFixture<ManagerPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ManagerPageComponent ]
     })
--- src/app/features/website/questions-management/question-edit/question-edit.component.spec.ts
+++ src/app/features/website/questions-management/question-edit/question-edit.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { QuestionEditComponent } from './question-edit.component';
 
@@ -6,7 +6,7 @@ describe('QuestionEditComponent', () => {
   let component: QuestionEditComponent;
   let fixture: ComponentFixture<QuestionEditComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ QuestionEditComponent ]
     })
